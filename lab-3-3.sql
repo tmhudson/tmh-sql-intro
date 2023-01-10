8c08154a852ce1c8974d05417921701dfcdd619c
tmhudson <mclean.hudson@kellogg.northwestern.edu>
2023-01-10 22:27:22 +0000

    Labs 1-1 thru 4-1

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -1,6 +1,12 @@
 -- What were the winningest teams in each season of the 
 -- modern era (from 1960-present), listed by winningest teams first?
 
+SELECT year, name, MAX(wins)
+FROM teams
+WHERE year > 1959
+GROUP BY year
+ORDER BY wins DESC;
+
 -- Expected result: 61 rows, starting with
 --
 -- +------+-------------------------------+-----------+
--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,11 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT first_name, last_name, SUM(hits)
+FROM players INNER JOIN stats ON players.id = stats.player_id
+WHERE  first_name = "Barry"
+AND last_name = "Bonds";
+
+
 -- Expected result:
 -- 2935
 
